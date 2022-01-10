6bd8a34ba5974be592f30f0da40ef3e30cfbb38c
Cvetlahka <Cvetlahka@yandex.ru>
2022-01-10 11:11:10 +0300

    final

--- trafficLite/ViewController.swift
+++ trafficLite/ViewController.swift
@@ -8,12 +8,39 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowWiew: UIView!
+    
+    @IBOutlet var greenWiew: UIView!
+    @IBOutlet var trafficButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.layer.cornerRadius = 0.5
+        redView.alpha = 0.3
+        yellowWiew.layer.cornerRadius = 0.5
+        yellowWiew.alpha = 0.3
+        greenWiew.layer.cornerRadius = 0.5
+        greenWiew.alpha = 0.3
+        
+        trafficButton.layer.cornerRadius = 0.2
+        
     }
 
-
+    @IBAction func pushButton(_ sender: Any) {
+        trafficButton.setTitle("NEXT", for: .normal)
+        
+        if redView.alpha == 1 {
+            redView.alpha = 0.3
+            yellowWiew.alpha = 1
+        }else if yellowWiew.alpha == 1{
+            yellowWiew.alpha = 0.3
+            greenWiew.alpha = 1
+        } else {
+            greenWiew.alpha = 0.3
+            redView.alpha = 1
+            
+        }
+    }
+    
 }
 
